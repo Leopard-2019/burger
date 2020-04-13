253c88d1d8eb6dde85f8c0418fa343af9c519fb1
Miguel Galarraga <miguelangelgalarraga@gmail.com>
2020-04-13 14:43:16 -0400

    include .gitigonre file

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ const mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
-			host: "localhost",
-			user: "root",
-			password: "Toronbolo777",
-			database: "burgers_db"
-		});
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Toronbolo777",
+    database: "osm1522mdgtvrsxk"
+  });
 };
 
 
